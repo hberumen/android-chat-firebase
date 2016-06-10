083afbc722af4c9eb8f23f0a6759522c06509d58
hberumen <hberumen@uaz.edu.mx>
2016-06-10 14:21:44 -0500

    Implementacion del presentador

--- app/src/main/java/hberumen/me/androidchat/login/LoginInteractor.java
+++ app/src/main/java/hberumen/me/androidchat/login/LoginInteractor.java
@@ -3,7 +3,7 @@ package hberumen.me.androidchat.login;
 /**
  * Created by hberumen on 09/06/16.
  */
-public interface LginInteractor {
+public interface LoginInteractor {
     void checkSession();
     void doSignUp(String email, String password);
     void doSignIn(String email, String password);
---
+++ app/src/main/java/hberumen/me/androidchat/login/LoginPresenterImpl.java
@@ -0,0 +1,77 @@
+package hberumen.me.androidchat.login;
+
+/**
+ * Created by hberumen on 10/06/16.
+ */
+public class LoginPresenterImpl implements LoginPresenter {
+
+    LoginView loginView;
+    LoginInteractor loginInteractor;
+
+    public LoginPresenterImpl(LoginView loginView) {
+        this.loginView = loginView;
+    }
+
+    @Override
+    public void onDestroy() {
+        loginView = null;
+    }
+
+    @Override
+    public void checkForAuthenticatedUser() {
+        if(loginView != null){
+            loginView.disableInputs();
+            loginView.showProgress();
+        }
+
+        loginInteractor.checkSession();
+    }
+
+    @Override
+    public void validateLogin(String email, String password) {
+        if(loginView != null){
+            loginView.disableInputs();
+            loginView.showProgress();
+        }
+
+        loginInteractor.doSignIn(email,password);
+    }
+
+    @Override
+    public void registerNewUser(String email, String password) {
+        if(loginView != null){
+            loginView.disableInputs();
+            loginView.showProgress();
+        }
+
+        loginInteractor.doSignUp(email, password);
+    }
+
+    private void onSignInSuccess(){
+        if(loginView != null){
+            loginView.navigateToMainScreen();
+        }
+    }
+
+    private void onSignUpSuccess(){
+        if(loginView != null){
+            loginView.newUserSuccess();
+        }
+    }
+
+    private void onSignInError(String error){
+        if(loginView != null){
+            loginView.enableInputs();
+            loginView.hideProgress();
+            loginView.loginError(error);
+        }
+    }
+
+    private void onSignUpError(String error){
+        if(loginView != null){
+            loginView.enableInputs();
+            loginView.hideProgress();
+            loginView.newUserError(error);
+        }
+    }
+}
