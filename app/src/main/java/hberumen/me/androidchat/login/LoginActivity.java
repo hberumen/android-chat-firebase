ba8cb0e22c103147c1c73d86726ac99f31c80bfc
hberumen <hberumen@uaz.edu.mx>
2016-06-10 14:09:49 -0500

    Implementacion de la vista

--- app/src/main/java/hberumen/me/androidchat/login/LoginActivity.java
+++ app/src/main/java/hberumen/me/androidchat/login/LoginActivity.java
@@ -1,12 +1,37 @@
 package hberumen.me.androidchat.login;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import hberumen.me.androidchat.R;
+import hberumen.me.androidchat.contactlist.ContactListActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements LoginView{
+
+    @BindView(R.id.btnSignin)
+    Button btnSignin;
+    @BindView(R.id.btnSignup)
+    Button btnSignup;
+    @BindView(R.id.editTxtEmail)
+    EditText editTxtEmail;
+    @BindView(R.id.editTxtPassword)
+    EditText editTxtPassword;
+    @BindView(R.id.progresBar)
+    ProgressBar progressBar;
+    @BindView(R.id.layoutMainContainer)
+    RelativeLayout container;
+
+    private LoginPresenter presenter;
 
 
     @Override
@@ -14,5 +39,70 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+    }
+
+    @Override
+    public void enableInputs() {
+        setInputs(true);
+    }
+
+    @Override
+    public void disableInputs() {
+        setInputs(false);
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @OnClick(R.id.btnSignup)
+    @Override
+    public void handleSignUp() {
+        presenter.registerNewUser(editTxtEmail.getText().toString(),
+                editTxtPassword.getText().toString());
+    }
+
+    @OnClick(R.id.btnSignin)
+    @Override
+    public void handleSignIn() {
+        presenter.validateLogin(editTxtEmail.getText().toString(),
+                editTxtPassword.getText().toString());
+    }
+
+    @Override
+    public void navigateToMainScreen() {
+        startActivity(new Intent(this, ContactListActivity.class));
+    }
+
+    @Override
+    public void loginError(String error) {
+        editTxtPassword.setText("");
+        String msgError = String.format(getString(R.string.login_error_message_signin), error);
+        editTxtPassword.setError(msgError);
+    }
+
+    @Override
+    public void newUserSuccess() {
+        Snackbar.make(container, R.string.login_notice_message_signup, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void newUserError(String error) {
+        editTxtPassword.setText("");
+        String msgError = String.format(getString(R.string.login_error_message_signup), error);
+        editTxtPassword.setError(msgError);
+    }
+
+    private void setInputs(boolean ebabled){
+        editTxtEmail.setEnabled(ebabled);
+        editTxtPassword.setEnabled(ebabled);
+        btnSignin.setEnabled(ebabled);
+        btnSignup.setEnabled(ebabled);
     }
 }
